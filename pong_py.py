89bb11e49680e0f62ac9c8157223f4b7c8f7c75e
Christopher Silva <131322948+ChrisFsilva@users.noreply.github.com>
2025-05-10 16:10:03 -0300

    ADD velocidade variavel
    A velocidade aumenta conforme você acerta a bola
    Foi incluído também um condigo que faria a bola alterar de cor conforme superasse determinada velocidade porem esse código esta apresentando erro e foi comentado

--- pong_py.py
+++ pong_py.py
@@ -7,6 +7,7 @@ janela =  pygame.display.set_mode([500,500])
 # Cores do display
 BG_Ground = (107, 142, 35) 
 PRETO = (0,0,0)
+Color_ball = (0,0,0)
 
 # Posições iniciais
 # Posição da raquete 2
@@ -46,7 +47,7 @@ def desenhar():
     # Cor da raquete 2
     pygame.draw.rect(janela, PRETO, (raquete2_x,raquete2_y,raquete_largura,raquete_altura))
     # Cor da bola
-    pygame.draw.ellipse(janela, PRETO, (bola_x,bola_y,bola_largura, bola_altura))
+    pygame.draw.ellipse(janela, Color_ball, (bola_x,bola_y,bola_largura, bola_altura))
     
     # Cor do placara
     placar_texto = font.render(f'{placar1} | {placar2}', True, PRETO)
@@ -82,6 +83,12 @@ while loop:
     bola_x += velocidade_bola_x
     bola_y += velocidade_bola_y
 
+    # Alterar cor da bola
+    # if velocidade_bola_x < 0.5 and velocidade_bola_y < 0.5:
+    #     Color_ball == (0,0,0)
+    # else:
+    #     Color_ball == (217,14,14)
+
 # Impedir a bola de sair
 
     if bola_y <= 0 or bola_y >= 498 - bola_altura:
@@ -92,11 +99,14 @@ while loop:
     # Colisão da raquete 1
     if (raquete1_x < bola_x < raquete1_x + raquete_largura) and (raquete1_y < bola_y < raquete1_y + raquete_altura):
         velocidade_bola_x = -velocidade_bola_x
-
+        velocidade_bola_x = velocidade_bola_x+0.03
+        velocidade_bola_y = velocidade_bola_y+0.03
 
     # Colisão da raquete 2
-    if (raquete2_x-10 < bola_x < raquete2_x + raquete_largura) and (raquete2_y < bola_y < raquete2_y + raquete_altura):
+    if (raquete2_x-13 < bola_x < raquete2_x + raquete_largura) and (raquete2_y < bola_y < raquete2_y + raquete_altura):
         velocidade_bola_x = -velocidade_bola_x
+        velocidade_bola_x = velocidade_bola_x+0.03
+        velocidade_bola_y = velocidade_bola_y+0.03
 
     
 # Pontuação
@@ -104,11 +114,13 @@ while loop:
     if bola_x <= 0:
         placar2 += 1
         bola_x, bola_y = 250,250
+        velocidade_bola_x,velocidade_bola_y = 0.1, 0.1
 
 # Jogador 2
     if bola_x >= 500:
         placar1 += 1
         bola_x, bola_y = 250,250
+        velocidade_bola_x,velocidade_bola_y = 0.1, 0.1
 
     desenhar()
 
